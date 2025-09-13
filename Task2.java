9aa1eaeaeaab6c81ad69c0b55ebc9c081f91a4f4
loretotarroza-cyber <loretotarroza@gmail.com>
2025-09-13 15:51:28 +0800

    Commit

---
+++ Task2.java
@@ -0,0 +1,13 @@
+public static void main(String[] args) {
+        char h ='H';
+        short shorty = 311;
+        byte zero = 0;
+        char W = 'w';
+        char R = 'r';
+        byte one = 1;
+        char d = 'd';
+        double ab = 2.0;
+        boolean fmt = true;
+        String output = "" + h + shorty + zero + " " + W + zero + R + one + d + " " + ab + " " + fmt;
+        System.out.println(output);
+        }
