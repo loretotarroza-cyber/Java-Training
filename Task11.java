e8af06ae931bc9f259fed7cee76bc6803260b653
loretotarroza-cyber <loretotarroza@gmail.com>
2025-09-20 18:36:31 +0800

    Commit

--- Task11.java
+++ Task11.java
@@ -1,23 +1,22 @@
+package test;
+
 import java.util.*;
 
 public class Task11 {
-
-}
-
-class test{
-
     public static void main(String[] args) {
-        
+        // Instantiate three books
         Book b1 = new Book("Java Programming", "John Smith", 2021, 39.99);
         Book b2 = new Book("Python Basics", "Alice Johnson", 2019, 29.99);
         Book b3 = new Book("C++ Advanced", "Robert Brown", 2022, 49.99);
 
-        System.out.println("Book 1:\n" + b1.toString() + "\n");
-        System.out.println("Book 2:\n" + b2.toString() + "\n");
-        System.out.println("Book 3:\n" + b3.toString() + "\n");
+        // Print details of each book
+        System.out.println("Book 1:\n" + b1 + "\n");
+        System.out.println("Book 2:\n" + b2 + "\n");
+        System.out.println("Book 3:\n" + b3 + "\n");
     }
 }
-    class Book {
+
+class Book {
     private String title;
     private String author;
     private int yearPublished;
@@ -29,6 +28,7 @@ class test{
         this.yearPublished = yearPublished;
         this.price = price;
     }
+
     @Override
     public String toString() {
         return "Title: \"" + title + "\"\n" +
